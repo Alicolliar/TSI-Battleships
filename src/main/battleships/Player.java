13c121ec994b28b7abe9bf00fafcdc78a2373b64
Aligallaton <alicolliar@gmail.com>
2022-05-02 05:57:23 +0100

    Bringing up to date

---
+++ src/main/battleships/Player.java
@@ -0,0 +1,17 @@
+package battleships
+
+public class Player {
+  private String playerName;
+
+  public Player(newPlayerName) {
+    playerName = newPlayerName;
+  }
+
+  public String getName()  {
+    return playerName;
+  }
+
+  public String toString() {
+    return playerName;
+  }
+}
